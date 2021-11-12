476e69f401d9cdba3b6c01be4febe224f8a86577
hary rabarijoely <rabonia@hotmail.com>
2021-11-12 16:04:18 +0100

    first commit

---
+++ app/models/event.rb
@@ -0,0 +1,15 @@
+class Event < ApplicationRecord
+  validates :start_date, presence: true
+  validates :duration, presence: true, :numericality => { :greater_the_or_equal_to => 0}
+  validate :divisible_by_five
+  validates :title, presence:true, length: { in: 5..10 }
+  validates :price, presence: true
+  validates_inclusion_of :price, :in => 1..1000
+  validates :location, presence: true
+
+  def divisible_by_five
+    if (self.duration % 5) != 0
+      self.errors[:base] << "Number must be divisible by 5!"
+    end
+  end
+end
